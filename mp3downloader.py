1e13d8ed7dc9a98c6363ff918d2fcf956154c615
KissMartin <98750670+KissMartin@users.noreply.github.com>
2024-02-27 17:13:52 +0100

    Update mp3downloader.py

--- mp3downloader.py
+++ mp3downloader.py
@@ -1,14 +1,48 @@
 import os
 from pytube import YouTube
 
+def get_valid_link() -> str:
+    while True:
+        try:
+            link = input("Music Link: ")
+            if link.lower() == 'exit':
+                return link
+            yt = YouTube(link)
+            return link
+        except Exception as e:
+            print("Invalid link. Please provide a valid YouTube link.")
+
+def get_valid_directory(title) -> str:
+    print(f"Downloading:\n'{title}' | to the current directory.")
+    print("Press Enter to confirm or type a new path to change the destination.")
+    print("Example: C:\\Users\\Music")
+    while True:
+        try:
+
+            destination = str(input(">> ") or '.')
+
+            if not os.path.exists(destination) or not os.path.isdir(destination):
+                raise ValueError("Invalid destination directory. Please provide a valid path.")
+            
+            return destination
+        except ValueError as ve:
+            print(ve)
 
 def main() -> None:
-    link = input("Link: ")
+    print("Type 'exit' to exit the program.")
+    link = get_valid_link()
+
+    if link.lower() == 'exit':
+        print("Exiting MP3 Downloader.")
+        return
+
     yt = YouTube(link)
 
     video = yt.streams.filter(only_audio=True).first()
 
-    destination = str(input(">> ")) or '.'
+    destination = get_valid_directory(yt.title)
+
+    print("Downloading...")
 
     out_file = video.download(output_path=destination)
 
@@ -16,8 +50,7 @@ def main() -> None:
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
 
-    print(yt.title + " has been successfully downloaded.")
-
+    print(yt.title + "\nHas been successfully downloaded to " + destination)
 
 if __name__ == "__main__":
     main()
